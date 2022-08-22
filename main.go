119b224b96607432d37a4bf150f52710277ea642
woods-one <0820ricer@gmail.com>
2022-08-22 14:31:09 +0900

    Received Json data and made it possible to send it to the database

--- main.go
+++ main.go
@@ -1,41 +1,59 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "net/http"
-    "github.com/labstack/echo"
+	"context"
+	"fmt"
+	"net/http"
 
-    firebase "firebase.google.com/go"
-    "google.golang.org/api/option"
+	firebase "firebase.google.com/go"
+	"github.com/labstack/echo"
+	"google.golang.org/api/option"
 )
 
 func main() {
 
-    ctx := context.Background()
-    opt := option.WithCredentialsFile("path/to/serviceAccountKey.json")
-    app, err := firebase.NewApp(ctx, nil, opt)
-    if err != nil {
-        fmt.Println(err)
-    }
+	ctx := context.Background()
+	opt := option.WithCredentialsFile("path/to/serviceAccountKey.json")
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    client, err := app.Firestore(ctx)
+	client, err := app.Firestore(ctx)
 
-    if err != nil {
-        fmt.Println(err)
-    }
-    e := echo.New()
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    e.GET("/add", func(c echo.Context) error {
-        _, _, err = client.Collection("Users").Add(ctx, map[string]interface{}{
-            "name": "First User",
-            "age": 11,
-            "email": "first@example.com",
-        })
-        if err != nil {
-            fmt.Println(err)
-        }
-        return c.String(http.StatusOK, "Hello World...!")
-    })
-    e.Logger.Fatal(e.Start(":8080"))
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World...!")
+	})
+
+	type Memo struct {
+		Name     string `json:"name"`
+		Birthday string `json:"birthday"`
+	}
+
+	e.POST("/addmemo", func(c echo.Context) error {
+		param := new(Memo)
+		if err := c.Bind(param); err != nil {
+			return err
+		}
+
+		_, _, err = client.Collection("Users").Add(ctx, map[string]interface{}{
+			"name":     param.Name,
+			"birthday": param.Birthday,
+		})
+		if err != nil {
+			fmt.Println(err)
+		}
+		p := &Memo{
+			Name:     "AC_TLE",
+			Birthday: "1/22",
+		}
+		return c.JSON(http.StatusOK, p)
+	})
+	e.Logger.Fatal(e.Start(":8080"))
 }
