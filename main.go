a732c48d43b73514eb77a44cfcf4583993059d11
woods-one <0820ricer@gmail.com>
2022-08-22 16:19:53 +0900

    Enabled automatic generation of documents and acquisition of names

--- main.go
+++ main.go
@@ -1,41 +1,62 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "net/http"
-    "github.com/labstack/echo"
+	"context"
+	"fmt"
+	"net/http"
 
-    firebase "firebase.google.com/go"
-    "google.golang.org/api/option"
+	"github.com/labstack/echo"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
 )
 
 func main() {
 
-    ctx := context.Background()
-    opt := option.WithCredentialsFile("path/to/serviceAccountKey.json")
-    app, err := firebase.NewApp(ctx, nil, opt)
-    if err != nil {
-        fmt.Println(err)
-    }
+	ctx := context.Background()
+	opt := option.WithCredentialsFile("path/to/serviceAccountKey.json")
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    client, err := app.Firestore(ctx)
+	client, err := app.Firestore(ctx)
 
-    if err != nil {
-        fmt.Println(err)
-    }
-    e := echo.New()
+	if err != nil {
+		fmt.Println(err)
+	}
+	e := echo.New()
 
-    e.GET("/add", func(c echo.Context) error {
-        _, _, err = client.Collection("Users").Add(ctx, map[string]interface{}{
-            "name": "First User",
-            "age": 11,
-            "email": "first@example.com",
-        })
-        if err != nil {
-            fmt.Println(err)
-        }
-        return c.String(http.StatusOK, "Hello World...!")
-    })
-    e.Logger.Fatal(e.Start(":8080"))
+	e.GET("/addmemo", func(c echo.Context) error {
+		_, _, err = client.Collection("Users").Add(ctx, map[string]interface{}{
+			"name":  "First User",
+			"age":   11,
+			"email": "first@example.com",
+		})
+		if err != nil {
+			fmt.Println(err)
+		}
+		return c.String(http.StatusOK, "Hello World...!")
+	})
+
+	type Room struct {
+		ID string `json:"id"`
+	}
+
+	e.GET("/createroom", func(c echo.Context) error {
+		ref := client.Collection("Users").NewDoc()
+
+		_, err := ref.Set(ctx, map[string]interface{}{})
+
+		if err != nil {
+			fmt.Println("An error has occurred: %s", err)
+		}
+
+		p := &Room{
+			ID: ref.ID,
+		}
+
+		return c.JSON(http.StatusOK, p)
+	})
+	e.Logger.Fatal(e.Start(":8080"))
 }
